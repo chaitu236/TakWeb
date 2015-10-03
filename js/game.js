0fba67acdf85d319db9c96be9aa1fe41f6d3c1e9
Chaitanya Vadrevu <chaitanya973@gmail.com>
2015-10-03 18:46:47 -0500

    created board

---
+++ js/game.js
@@ -0,0 +1,51 @@
+var camera, scene, renderer;
+var mesh;
+
+init();
+animate();
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.z = 400;
+
+  scene = new THREE.Scene();
+
+  var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
+
+  var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+  var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  //
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
